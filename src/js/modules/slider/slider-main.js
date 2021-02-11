6e8bf37bebc4d1031a1272b1669b967fa24b66e8
Mike <mi5hqaaaaov@gmail.com>
2021-02-11 13:13:09 +0200

    sliders

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,12 +1,9 @@
-export default class Slider {
-   constructor(page,btns) {
-      this.page = document.querySelector(page);
-      this.slides = this.page.children;
-      this.btns = document.querySelectorAll(btns);
-      this.slideIndex = 1;
+import Slider from './slider';
 
+export default class MainSlider extends Slider {
+   constructor(btns) {
+      super(btns);
    }
-
 
    showSlides(n) {
       if (n > this.slides.length) {
@@ -19,7 +16,8 @@ export default class Slider {
 
       try {
          this.hanson.style.opacity = '0';
-         if (n === 3) {
+
+         if (n == 3) {
             this.hanson.classList.add('animated');
             setTimeout(() => {
                this.hanson.style.opacity = '1';
@@ -30,13 +28,11 @@ export default class Slider {
          }
       } catch (e) { }
 
-
       this.slides.forEach(slide => {
          slide.style.display = 'none';
       });
 
       this.slides[this.slideIndex - 1].style.display = 'block';
-      this.slides[this.slideIndex - 1].classList.add('animated','fadeInDown');
    }
 
    plusSlides(n) {
@@ -48,12 +44,12 @@ export default class Slider {
          this.hanson = document.querySelector('.hanson');
       } catch (e) { }
 
-      this.btns.forEach(btn => {
-         btn.addEventListener('click',() => {
+      this.btns.forEach(item => {
+         item.addEventListener('click',() => {
             this.plusSlides(1);
          });
 
-         btn.parentNode.previousElementSibling.addEventListener('click',(e) => {
+         item.parentNode.previousElementSibling.addEventListener('click',(e) => {
             e.preventDefault();
             this.slideIndex = 1;
             this.showSlides(this.slideIndex);
@@ -62,6 +58,4 @@ export default class Slider {
 
       this.showSlides(this.slideIndex);
    }
-
 }
-
